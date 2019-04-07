e3ff052e6bae01e386041ce32706fc17098c1e8d
Konstantinov Oleg <23136372+oskonstantinov@users.noreply.github.com>
2019-04-07 17:01:50 +0300

    Add methods

--- sandbox/src/main/java/ru/stqa/pft/sandbox/First.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/First.java
@@ -3,7 +3,25 @@ package ru.stqa.pft.sandbox;
 public class First {
 
   public static void main(String[] args) {
-    System.out.println("Hello, world");
+    hello("user");
+
+    double l = 7;
+    System.out.println("Площадь квадрата со стороной " + l + " равна " + area(l));
+
+    double a = 3;
+    double b = 8;
+    System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " равна " + area(a, b));
   }
 
+  public static void hello(String somebody) {
+    System.out.println("Hello, " + somebody + "!");
+  }
+
+  public static double area(double l){
+    return l * l;
+  }
+
+  public static double area(double a, double b) {
+    return a * b;
+  }
 }
