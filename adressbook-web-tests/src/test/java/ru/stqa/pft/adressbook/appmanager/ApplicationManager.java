ab58a045e7e15de9c4174a0292c894589a63a06b
Konstantinov Oleg <23136372+oskonstantinov@users.noreply.github.com>
2019-04-20 13:43:57 +0300

    Add driver browser options

--- adressbook-web-tests/src/test/java/ru/stqa/pft/adressbook/appmanager/ApplicationManager.java
+++ adressbook-web-tests/src/test/java/ru/stqa/pft/adressbook/appmanager/ApplicationManager.java
@@ -1,7 +1,10 @@
 package ru.stqa.pft.adressbook.appmanager;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.BrowserType;
 
 import java.util.concurrent.TimeUnit;
 
@@ -13,9 +16,20 @@ public class ApplicationManager {
   private GroupHelper groupHelper;
   private SessionHelper sessionHelper;
   private ContactHelper contactHelper;
+  private String browser;
+
+  public ApplicationManager(String browser) {
+    this.browser = browser;
+  }
 
   public void init() {
-    wd = new FirefoxDriver();
+    if (browser == BrowserType.FIREFOX) {
+      wd = new FirefoxDriver();
+    } else if (browser == BrowserType.CHROME) {
+      wd = new ChromeDriver();
+    } else if (browser == BrowserType.IE) {
+      wd = new InternetExplorerDriver();
+    }
     wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     groupHelper = new GroupHelper(wd);
     contactHelper = new ContactHelper(wd);
--- adressbook-web-tests/src/test/java/ru/stqa/pft/adressbook/tests/TestBase.java
+++ adressbook-web-tests/src/test/java/ru/stqa/pft/adressbook/tests/TestBase.java
@@ -1,12 +1,13 @@
 package ru.stqa.pft.adressbook.tests;
 
+import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import ru.stqa.pft.adressbook.appmanager.ApplicationManager;
 
 public class TestBase {
   // "final" value is assigned only once
-  protected final ApplicationManager app = new ApplicationManager();
+  protected final ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
 
   @BeforeMethod(alwaysRun = true)
   public void setUp() {
