6c0cf644d50fe6ba62b042fc105d16e31e1bd31b
Konstantinov Oleg <23136372+oskonstantinov@users.noreply.github.com>
2019-05-10 15:38:27 +0300

    Added .xml data support

--- adressbook-web-tests/src/test/java/ru/stqa/pft/adressbook/model/GroupData.java
+++ adressbook-web-tests/src/test/java/ru/stqa/pft/adressbook/model/GroupData.java
@@ -1,9 +1,14 @@
 package ru.stqa.pft.adressbook.model;
 
+import com.thoughtworks.xstream.annotations.XStreamAlias;
+import com.thoughtworks.xstream.annotations.XStreamOmitField;
+
 import java.util.Objects;
 
+@XStreamAlias("group")
 public class GroupData {
   // "final" value is assigned only once
+  @XStreamOmitField
   private int id = Integer.MAX_VALUE;
   private String name;
   private String header;
