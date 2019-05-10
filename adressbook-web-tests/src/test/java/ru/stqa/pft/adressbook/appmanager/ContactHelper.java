0ca18b6baf7167da80820a5162b2a3d7929a1852
Konstantinov Oleg <23136372+oskonstantinov@users.noreply.github.com>
2019-05-10 17:44:40 +0300

    Exercize 13 with photo

--- adressbook-web-tests/src/test/java/ru/stqa/pft/adressbook/appmanager/ContactHelper.java
+++ adressbook-web-tests/src/test/java/ru/stqa/pft/adressbook/appmanager/ContactHelper.java
@@ -7,6 +7,8 @@ import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import ru.stqa.pft.adressbook.model.ContactData;
 import ru.stqa.pft.adressbook.model.Contacts;
+
+import java.io.File;
 import java.util.List;
 
 public class ContactHelper extends HelperBase {
@@ -68,6 +70,7 @@ public class ContactHelper extends HelperBase {
 
   public void create(ContactData contact) {
     fillContactForm(contact, true);
+    attach(By.name("photo"), new File("src/test/resources/sheldon.jpg"));
     submitContactCreation();
     contactCache = null;
     returnToHomePage();
