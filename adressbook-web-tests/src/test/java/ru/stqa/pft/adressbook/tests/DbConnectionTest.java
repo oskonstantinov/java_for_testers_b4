0a3a1643eaac5bbbe6bb195d7f02d85cb00e9494
Konstantinov Oleg <23136372+oskonstantinov@users.noreply.github.com>
2019-05-17 12:36:23 +0300

    Added database connector

---
+++ adressbook-web-tests/src/test/java/ru/stqa/pft/adressbook/tests/DbConnectionTest.java
@@ -0,0 +1,41 @@
+package ru.stqa.pft.adressbook.tests;
+
+import org.testng.annotations.Test;
+import ru.stqa.pft.adressbook.model.GroupData;
+import ru.stqa.pft.adressbook.model.Groups;
+
+import java.sql.*;
+
+public class DbConnectionTest {
+
+  @Test
+  public void testDbConnection() {
+    Connection conn = null;
+    try {
+      conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/addressbook?"+
+              "user=root&password=&serverTimezone=UTC");
+      Statement st = conn.createStatement();
+      ResultSet resultSet = st.executeQuery("select group_id,group_name,group_header,group_footer from group_list");
+      Groups groups = new Groups();
+      while (resultSet.next()) {
+        groups.add(new GroupData().withId(resultSet.getInt("group_id"))
+                       .withName(resultSet.getString("group_name"))
+                       .withHeader(resultSet.getString("group_header"))
+                       .withFooter(resultSet.getString("group_footer")));
+      }
+      resultSet.close();
+      st.close();
+      conn.close();
+
+      System.out.println(groups);
+
+      // Do something with the Connection
+
+    } catch (SQLException ex) {
+      // handle any errors
+      System.out.println("SQLException: " + ex.getMessage());
+      System.out.println("SQLState: " + ex.getSQLState());
+      System.out.println("VendorError: " + ex.getErrorCode());
+    }
+  }
+}
