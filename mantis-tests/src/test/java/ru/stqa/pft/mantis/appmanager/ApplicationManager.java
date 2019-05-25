7933d2a89897ede7ff67a5fff4ea2b1d931e9ce1
Konstantinov Oleg <23136372+oskonstantinov@users.noreply.github.com>
2019-05-25 18:30:05 +0300

    Exercize 17

---
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
@@ -0,0 +1,46 @@
+package ru.stqa.pft.mantis.appmanager;
+
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.BrowserType;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+public class ApplicationManager {
+  private final Properties properties;
+  // "protected" method or variable is available for class, package and subclass
+  protected WebDriver wd;
+  private String browser;
+
+  public ApplicationManager(String browser) {
+    this.browser = browser;
+    properties = new Properties();
+  }
+
+  public void init() throws IOException {
+    String target = System.getProperty("target", "local");
+    properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
+
+    if (browser.equals(BrowserType.FIREFOX)) {
+      wd = new FirefoxDriver();
+    } else if (browser.equals(BrowserType.CHROME)) {
+      wd = new ChromeDriver();
+    } else if (browser.equals(BrowserType.IE)) {
+      wd = new InternetExplorerDriver();
+    }
+    wd.manage().window().setSize(new Dimension(1280, 1024));
+    wd.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+    wd.get(properties.getProperty("web.baseUrl"));
+  }
+
+  public void stop() {
+    wd.quit();
+  }
+}
