c40d23371c10ecf6b592ac16f35f9c1428d9c380
Konstantinov Oleg <23136372+oskonstantinov@users.noreply.github.com>
2019-05-26 17:00:55 +0300

    Added receiving mails by localhost

---
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/HelperBase.java
@@ -0,0 +1,64 @@
+package ru.stqa.pft.mantis.appmanager;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+
+import java.io.File;
+
+public class HelperBase {
+  // "protected" method or variable is available for class, package and subclass
+  protected ApplicationManager app;
+  protected WebDriver wd;
+
+  public HelperBase(ApplicationManager app) {
+    // "this" represents the current class instance
+    this.app = app;
+    this.wd = app.getDriver();
+  }
+
+  protected void click(By locator) {
+    wd.findElement(locator).click();
+  }
+
+  protected void type(By locator, String text) {
+    click(locator);
+    if (text != null) {
+      String existingText =  wd.findElement(locator).getAttribute("value");
+      if (! text.equals(existingText)) {
+        wd.findElement(locator).clear();
+        wd.findElement(locator).sendKeys(text);
+      }
+    }
+  }
+
+  protected void attach(By locator, String filepath) {
+    if (filepath != null) {
+      File file = new File(filepath);
+      wd.findElement(locator).sendKeys(file.getAbsolutePath());
+    }
+  }
+
+  protected boolean isAlertPresent() {
+    try {
+      wd.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+
+  protected void alertAccept() {
+    wd.switchTo().alert().accept();
+  }
+
+  protected boolean isElementPresent(By locator) {
+    try {
+      wd.findElement(locator);
+      return true;
+    } catch (NoSuchElementException ex) {
+      return false;
+    }
+  }
+}
