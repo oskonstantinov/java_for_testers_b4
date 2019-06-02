e010b2f4f751aa7ed9a3b5897cf347fb7f994747
Konstantinov Oleg <23136372+oskonstantinov@users.noreply.github.com>
2019-06-02 13:49:39 +0300

    Added sample, worked by GitHub api

---
+++ github-sample/src/test/java/ru/stqa/pft/github/GithubTests.java
@@ -0,0 +1,19 @@
+package ru.stqa.pft.github;
+
+import com.google.common.collect.ImmutableMap;
+import com.jcabi.github.*;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+public class GithubTests {
+
+  @Test
+  public void testCommits() throws IOException {
+    Github github = new RtGithub("9f39f89cd470e5c31dbfe8881da8f04482b111d4");
+    RepoCommits commits = github.repos().get(new Coordinates.Simple("oskonstantinov", "java_for_testers_b4")).commits();
+    for (RepoCommit commit : commits.iterate(new ImmutableMap.Builder<String, String>().build())) {
+      System.out.println(new RepoCommit.Smart(commit).message());
+    }
+  }
+}
